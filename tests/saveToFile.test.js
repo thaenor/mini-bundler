b6431cf845728680af3dda1914803f4a793cab2f
Francisco Santos <thaenor@users.noreply.github.com>
2019-05-03 17:19:28 +0100

    Update saveToFile.test.js

--- tests/saveToFile.test.js
+++ tests/saveToFile.test.js
@@ -7,9 +7,9 @@ afterAll(() => {
   fs.unlinkSync(path.resolve('./tests/fixtures/bundle.js'));
 });
 
-test('can save data to a file', () => {
+ftest('can save data to a file', () => {
   const entryData = 'success';
   saveToFile(entryData, 'tests/fixtures');
-  const contenstOfFile = readFile('bundle.js', 'tests/fixtures').toString();
-  expect(contenstOfFile).toEqual(entryData);
+  const contentsOfFile = readFile('bundle.js', 'tests/fixtures').toString();
+  expect(contentsOfFile).toEqual(entryData);
 });
